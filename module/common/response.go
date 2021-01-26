6293aa04f8d986fa9b976a704c614c09e5325949
Nam Pham <nam.phho@gmail.com>
2021-01-26 07:47:15 +0700

    add error json standart

--- module/common/response.go
+++ module/common/response.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	"errors"
+	"net/http"
+)
+
 type successRes struct {
 	Data   interface{} `json:"data"`
 	Paging interface{} `json:"paging,omitempty"`
@@ -12,4 +17,49 @@ func NewSuccessResponse(data, paging, filter interface{}) *successRes {
 
 func SimpleSuccessResponse(data interface{}) *successRes {
 	return &successRes{data, nil, nil}
+}
+
+type AppError struct {
+	StatusCode int    `json:"status_code"`
+	RootErr    error  `json:"-"`
+	Message    string `json:"message"`
+	Log        string `json:"log"`
+	Key        string `json:"key"`
+}
+
+func NewErrorResponse(root error, message, log, key string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusBadRequest,
+		RootErr:    root,
+		Message:    message,
+		Log:        log,
+		Key:        key,
+	}
+}
+
+func NewUnauthorized(root error, message, key string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusUnauthorized,
+		RootErr:    root,
+		Message:    message,
+		Key:        key,
+	}
+}
+
+func NewCustomError(root error, message, key string) *AppError {
+	if root != nil {
+		return NewErrorResponse(root, message, root.Error(), key)
+	}
+	return NewErrorResponse(errors.New(message), message, message, key)
+}
+
+func (e *AppError) RootError() error {
+	if err, ok := e.RootErr.(*AppError); ok {
+		return err.RootErr
+	}
+	return e.RootErr
+}
+
+func (e *AppError) Error() string {
+	return e.RootErr.Error()
 }
