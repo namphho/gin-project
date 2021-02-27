de880b3f81deb44262b7c284265498c19f15a56d
Nam Pham <nam.phho@gmail.com>
2021-02-27 15:00:41 +0700

    change input of GetImageDimension to io.Reader

--- module/upload/uploadtransport/gin_upload.go
+++ module/upload/uploadtransport/gin_upload.go
@@ -9,9 +9,9 @@ import (
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
+	"io"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
 	"time"
 )
@@ -27,15 +27,20 @@ func Upload(appCtx appctx.AppContext) func(ctx *gin.Context){
 			panic(common.ErrInvalidRequest(err))
 		}
 
+		file, err := fileHeader.Open()
+		if err != nil {
+			panic(common.ErrInvalidRequest(err))
+		}
+
+		w, h, err := GetImageDimension(file)
+		if err != nil {
+			panic(uploadmodel.ErrNoFileConfig)
+		}
+
 		fileName := fmt.Sprintf("%d%s", time.Now().Nanosecond(), filepath.Ext(fileHeader.Filename))
 
 		if err := ctx.SaveUploadedFile(fileHeader, fmt.Sprintf(LOCAL_STORAGE_IMAGE_PATH, fileName)); err != nil {
 			panic(common.ErrInvalidRequest(err))
-		}
-
-		w, h, err := GetImageDimension(fmt.Sprintf(LOCAL_STORAGE_IMAGE_PATH, fileName))
-		if err != nil {
-			panic(uploadmodel.ErrNoFileConfig)
 		}
 
 		resp := common.Image{
@@ -50,15 +55,8 @@ func Upload(appCtx appctx.AppContext) func(ctx *gin.Context){
 	}
 }
 
-func GetImageDimension(imagePath string) (int, int, error) {
-	file, err := os.Open(imagePath)
-
-	if err != nil {
-		return 0, 0, err
-	}
-	defer file.Close()
-
-	config, _, err := image.DecodeConfig(file)
+func GetImageDimension(reader io.Reader) (int, int, error) {
+	config, _, err := image.DecodeConfig(reader)
 	if err != nil {
 		log.Printf("err: ", err)
 		return 0, 0, err
